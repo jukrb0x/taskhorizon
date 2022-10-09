8fbf78570700bf6584e88312ec1e98df9fdcd661
Jabriel <15688641+jukrb0x@users.noreply.github.com>
2022-10-09 17:02:53 +0800

    workflow: update `husky` and `commitlint` config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,29 @@
 module.exports = {
-    extends: ['@commitlint/config-conventional']
+    ignores: [(commit) => commit.includes('init')],
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'subject-empty': [2, 'never'],
+        'type-empty': [2, 'never'],
+        'type-enum': [
+            2,
+            'always',
+            [
+                'feat',
+                'fix',
+                'perf',
+                'style',
+                'docs',
+                'test',
+                'refactor',
+                'build',
+                'ci',
+                'chore',
+                'revert',
+                'wip',
+                'workflow',
+                'types',
+                'release'
+            ]
+        ]
+    }
 };
