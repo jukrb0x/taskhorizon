1eafd653449fbd0d38ca70c40dd80fe4b24e05b0
Jabriel <15688641+jukrb0x@users.noreply.github.com>
2022-10-21 20:23:19 +0800

    wip: minor changes

--- src/components/MainLayout/index.tsx
+++ src/components/MainLayout/index.tsx
@@ -1,21 +1,25 @@
 import { Button, Layout } from '@douyinfe/semi-ui';
 import MainRouter from '@/routers/MainRouter';
 import TodoApp from '@/pages/TodoApp';
+import Resizer from '@/components/Resizer';
+import { useState } from 'react';
+import './index.scss';
 
 export default function MainLayout() {
     const { Header, Footer, Content, Sider } = Layout;
+    const [siderWidth, setSiderWidth] = useState(300);
 
     return (
-        <Layout>
+        <Layout hasSider className={'layout-wrapper'}>
             <Sider>
                 <TodoApp />
             </Sider>
+            <Resizer /> {/* resize sider */}
             <Layout>
-                <Header>Header</Header>
+                <Header className={'header'}>Header</Header>
                 <Content>
                     <MainRouter />
                 </Content>
-                <Footer>Footer</Footer>
             </Layout>
         </Layout>
     );
