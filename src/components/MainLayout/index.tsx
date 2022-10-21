75a577975c933ab2f06162ba1db2b7f9b42f5c89
Jabriel <15688641+jukrb0x@users.noreply.github.com>
2022-10-21 20:39:02 +0800

    fix: `ConfigProvider` covers all semi-ui component

--- src/components/MainLayout/index.tsx
+++ src/components/MainLayout/index.tsx
@@ -1,26 +1,29 @@
-import { Button, Layout } from '@douyinfe/semi-ui';
+import { Button, ConfigProvider, Layout } from '@douyinfe/semi-ui';
 import MainRouter from '@/routers/MainRouter';
 import TodoApp from '@/pages/TodoApp';
 import Resizer from '@/components/Resizer';
 import { useState } from 'react';
 import './index.scss';
+import en_GB from '@douyinfe/semi-ui/lib/es/locale/source/en_GB';
 
 export default function MainLayout() {
     const { Header, Footer, Content, Sider } = Layout;
     const [siderWidth, setSiderWidth] = useState(300);
 
     return (
-        <Layout hasSider className={'layout-wrapper'}>
-            <Sider>
-                <TodoApp />
-            </Sider>
-            <Resizer /> {/* resize sider */}
-            <Layout>
-                <Header className={'header'}>Header</Header>
-                <Content>
-                    <MainRouter />
-                </Content>
+        <ConfigProvider locale={en_GB}>
+            <Layout hasSider className={'layout-wrapper'}>
+                <Sider>
+                    <TodoApp />
+                </Sider>
+                <Resizer /> {/* resize sider */}
+                <Layout>
+                    <Header className={'header'}>Header</Header>
+                    <Content>
+                        <MainRouter />
+                    </Content>
+                </Layout>
             </Layout>
-        </Layout>
+        </ConfigProvider>
     );
 }
--- src/pages/TodoApp/index.tsx
+++ src/pages/TodoApp/index.tsx
@@ -1,17 +1,13 @@
-import { ConfigProvider } from '@douyinfe/semi-ui';
-import en_GB from '@douyinfe/semi-ui/lib/es/locale/source/en_GB';
 import TodoList from '@/components/Todo/TodoList';
 
 export default function TodoApp() {
     return (
         <>
             {/* todo: ConfigProvider will be moved out to this component*/}
-            <ConfigProvider locale={en_GB}>
-                <h1>{'Todo App'}</h1>
-                <div style={{ margin: '5px' }}>
-                    <TodoList />
-                </div>
-            </ConfigProvider>
+            <h1>{'Todo App'}</h1>
+            <div style={{ margin: '5px' }}>
+                <TodoList />
+            </div>
         </>
     );
 }
