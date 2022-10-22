700d6eba8f59fc72ce5aacedd0908141b3e9a62f
Jabriel <15688641+jukrb0x@users.noreply.github.com>
2022-10-22 15:19:52 +0800

    chore: ignore `/public`

---
+++ src/routers/MainRouterProvider.tsx
@@ -0,0 +1,25 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import RouteError from '@/routers/RouteError';
+import CalendarView from '@/components/Calendar';
+import Calendar from '@/components/WIP/Calendar';
+import MainLayout from '@/components/MainLayout';
+
+export default function MainRouterProvider() {
+    const router = createBrowserRouter([
+        // todo: the routes now are shit
+        // separate them
+        {
+            path: '/',
+            element: <MainLayout />,
+            errorElement: <RouteError />,
+            children: [
+                {
+                    path: '/component/cal',
+                    element: <Calendar />
+                }
+            ]
+        }
+    ]);
+
+    return <RouterProvider router={router} />;
+}
