d6dc49650379415a002473548d0500bbbf47158a
Jabriel <15688641+jukrb0x@users.noreply.github.com>
2022-10-25 16:15:40 +0800

    fix: use tailwindcss without preflight `base`

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 module.exports = {
     content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
     theme: {
         extend: {}
     },
-    plugins: []
+    plugins: [],
+    prefix: 'tw-',
+    corePlugins: {
+        preflight: false
+    }
 };
