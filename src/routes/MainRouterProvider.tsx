882eb916d32bc1bea4f76042d5e24735c49bb71a
Jabriel <15688641+jukrb0x@users.noreply.github.com>
2022-10-25 18:31:17 +0800

    chore: typing hint

--- src/routes/MainRouterProvider.tsx
+++ src/routes/MainRouterProvider.tsx
@@ -7,6 +7,7 @@ import { routes } from './routes';
 
 export default function MainRouterProvider() {
     const router = createBrowserRouter(routes);
+    // tips: can use createRoutesFromElements(JSX.Element) => Route[]
 
     return <RouterProvider router={router} />;
 }
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,17 +2,16 @@ import RouteError from '@/routes/RouteError';
 import SemiCalendar from '@/components/SemiCalendar';
 import Calendar from '@/components/Calendar';
 import MainLayout from '@/components/MainLayout';
-import { Navigate } from 'react-router-dom';
+import { Navigate, RouteObject } from 'react-router-dom';
 
-export const routes = [
-    // todo: the routes now are shit
-    // separate them
+export const routes: RouteObject[] = [
     {
         path: '/',
         element: <MainLayout />,
         errorElement: <RouteError />,
         children: [
             {
+                // default redirection
                 path: '/',
                 element: <Navigate to={'/home'} />
             },
