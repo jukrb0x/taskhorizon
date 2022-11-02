5729ff0fda77275b3e7a750877c90c36cfee860a
Jabriel <15688641+jukrb0x@users.noreply.github.com>
2022-11-02 22:04:40 +0800

    wip: mark for issue #6

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
-import { ConfigProvider, Layout } from '@douyinfe/semi-ui';
+import { ConfigProvider as SemiConfigProvider, Layout } from '@douyinfe/semi-ui';
 import en_GB from '@douyinfe/semi-ui/lib/es/locale/source/en_GB';
 import Resizer from '@/components/Resizer';
 import DebugPanel from '@/routes/components/DebugPanel';
@@ -14,7 +14,7 @@ export default function Home() {
     const isDebug = import.meta.env.MODE === 'development';
 
     return (
-        <ConfigProvider locale={en_GB}>
+        <SemiConfigProvider locale={en_GB}>
             <Layout hasSider className={'main-layout-wrapper'}>
                 <Sider className={'sider'}>
                     <TodoApp />
@@ -23,6 +23,7 @@ export default function Home() {
                 <Layout>
                     <Header>Header</Header>
                     <Content>
+                        {/* fixme: router outlet here which is not clear, too far separate with the React Router itself*/}
                         <Outlet />
                     </Content>
                 </Layout>
@@ -31,6 +32,6 @@ export default function Home() {
                 /* debug panel only display with development mode*/
                 isDebug ? <DebugPanel /> : null
             }
-        </ConfigProvider>
+        </SemiConfigProvider>
     );
 }
--- src/routes/AppRouterProvider.tsx
+++ src/routes/AppRouterProvider.tsx
@@ -4,5 +4,6 @@ import { routes } from './routes';
 export default function AppRouterProvider() {
     const router = createBrowserRouter(routes);
     // tips: can use createRoutesFromElements(JSX.Element) => Route[]
+    // todo: use tsx template to create routes bcuz we cannot see the actual <App />
     return <RouterProvider router={router} />;
 }
