93cbc82591f470154e9bbf163ed12333232f670e
Jabriel <15688641+jukrb0x@users.noreply.github.com>
2022-11-02 20:32:50 +0800

    feat(tauri): async starting the app

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,11 @@
 #![cfg_attr(
-    all(not(debug_assertions), target_os = "windows"),
-    windows_subsystem = "windows"
+all(not(debug_assertions), target_os = "windows"),
+windows_subsystem = "windows"
 )]
+
+use std::error::Error;
+use tauri::{Manager, RunEvent};
+use tracing::{debug, error};
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -9,9 +13,53 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
-fn main() {
-    tauri::Builder::default()
+#[tauri::command(async)]
+async fn app_ready(app_handle: tauri::AppHandle) {
+    let window = app_handle.get_window("main").unwrap();
+    window.show().unwrap();
+    println!("app is ready");
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let app = tauri::Builder::default()
+        .setup(|app| {
+            let app = app.handle();
+
+            app.windows().iter().for_each(|(_, window)| {
+                window.hide().unwrap();
+
+                #[cfg(target_os = "windows")]
+                window.set_decorations(true).unwrap();
+
+                #[cfg(target_os = "macos")]
+                {
+                    // let window = window.ns_window().unwrap();
+                }
+            });
+
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .invoke_handler(tauri::generate_handler![app_ready])
+        .build(tauri::generate_context!())?;
+
+    app.run(move |app_handler, event| {
+        if let RunEvent::ExitRequested { .. } = event {
+            debug!("Closing all open windows...");
+            app_handler
+                .windows()
+                .iter()
+                .for_each(|(window_name, window)| {
+                    debug!("closing window: {window_name}");
+                    if let Err(e) = window.close() {
+                        error!("failed to close window '{}': {:#?}", window_name, e);
+                    }
+                });
+
+            app_handler.exit(0);
+        }
+    });
+
+    Ok(())
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import AppRouterProvider from '@/routes/AppRouterProvider';
 import './index.scss';
+import { invoke } from '@tauri-apps/api';
+import { useEffect } from 'react';
 
 function App() {
+    useEffect(() => {
+        // invoke Tauri to show up the window
+        invoke('app_ready'), [];
+    });
+
     return (
         <>
             <AppRouterProvider />
