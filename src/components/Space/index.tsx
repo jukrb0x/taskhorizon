4ca8beb3e2c8bcb817ecbef6a83b5faae5275193
Jabriel <15688641+jukrb0x@users.noreply.github.com>
2022-11-03 22:20:16 +0800

    fix: remove useless

--- src/components/Space/index.tsx
+++
@@ -1,7 +0,0 @@
-//         demo component
-// ==== DO NOT USE IT IN CODE ====+
-//       it is not efficient
-
-export const Space = ({ px }: { px: number }) => {
-    return <div style={{ margin: px }}></div>;
-};
