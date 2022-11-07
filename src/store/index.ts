415ec89cf29bab3b5297668dff592d8ce8ef36bd
Jabriel <15688641+jukrb0x@users.noreply.github.com>
2022-11-07 15:08:42 +0800

    chore: comments

--- src/store/index.ts
+++ src/store/index.ts
@@ -7,6 +7,7 @@ import { StateCreator } from 'zustand';
 
 // love to separate it out, but it's not working now
 // looks like have to "initialize" the middleware first
+// todo: understanding Zustand https://docs.pmnd.rs/zustand/getting-started/introduction
 
 // export const withMiddleware = (store: StateCreator<any>, name: string) => {
 //     return devtools(persist(store, { name: name }), {
