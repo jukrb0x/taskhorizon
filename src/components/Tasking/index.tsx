1d99d760429ae843ff5a0bb2fb7624b7601d82de
Jabriel <15688641+jukrb0x@users.noreply.github.com>
2022-11-13 05:53:41 +0800

    feat: auto animated todo list

--- src/components/Tasking/index.tsx
+++ src/components/Tasking/index.tsx
@@ -1,12 +1,18 @@
 import { TodoInput, TodoList } from '@/components/Tasking/components';
 import useTodoStore from '@/store/todo-store';
+import { useEffect, useRef } from 'react';
+import autoAnimate from '@formkit/auto-animate';
 
 export default function TodoApp() {
     const { todoList } = useTodoStore();
+    const appRef = useRef(null);
+    useEffect(() => {
+        appRef.current && autoAnimate(appRef.current);
+    }, [appRef]);
     return (
-        <>
+        <div ref={appRef}>
             <TodoInput />
             <TodoList todos={todoList} />
-        </>
+        </div>
     );
 }
