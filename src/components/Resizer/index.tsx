86a388bfd09375155993ac5e392022a403f381fc
Jabriel <15688641+jukrb0x@users.noreply.github.com>
2022-11-13 05:34:51 +0800

    feat: resize siderbar

--- src/components/Resizer/index.tsx
+++ src/components/Resizer/index.tsx
@@ -1,10 +1,14 @@
-import './index.scss';
 import { cls } from '@/utils';
+import { MouseEventHandler } from 'react';
 
-export default function Resizer() {
-    return (
-        <>
-            <div className={'resizer'}></div>
-        </>
-    );
+export default function Resizer({
+    onMouseDown,
+    onMouseUp
+}: {
+    onMouseDown: MouseEventHandler;
+    onMouseUp: MouseEventHandler;
+}) {
+    const ResizeBar = cls.div`tw-w-1.5 tw-h-full tw-cursor-col-resize
+     tw-z-50 tw-bg-gray-500/30 tw-opacity-50 hover:tw-opacity-100 tw-transition tw-ease-out tw-duration-300`;
+    return <ResizeBar onMouseDown={onMouseDown} onMouseUp={onMouseUp} />;
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useCallback, useRef, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import { ConfigProvider as SemiConfigProvider, Layout } from '@douyinfe/semi-ui';
 import en_GB from '@douyinfe/semi-ui/lib/es/locale/source/en_GB';
@@ -6,33 +6,69 @@ import Resizer from '@/components/Resizer';
 import TodoApp from '@/components/Tasking';
 import EventController from '@/components/BigCalendar/EventController';
 import { cls } from '@/utils';
+import { useEventListener, useSize } from 'ahooks';
 
 export default function Home() {
     const { Header, Footer, Content, Sider } = Layout;
-    const [siderWidth, setSiderWidth] = useState(300);
+    const [siderWidth, setSiderWidth] = useState(350);
+    const siderRef = useRef(null);
+    const [isResizing, setIsResizing] = useState(false);
 
-    const DragRegionOffset = cls.div`tw-h-5 tw-bg-red-100`;
+    // resize sidebar
+    useEventListener(
+        'mousemove',
+        useCallback(
+            ({ clientX }: MouseEvent) => {
+                if (isResizing) {
+                    if (clientX < 300 || clientX > 500) return;
+                    setSiderWidth(clientX);
+                }
+            },
+            [isResizing]
+        )
+    );
+
+    // useEventListener('mouseup', () => {
+    //     setIsResizing(false);
+    // });
+
+    const DragRegionOffsetWrapper = cls.div`tw-h-5 tw-bg-red-100`;
+    const dragRegionRef = useRef(null);
+    const windowWrapperRef = useRef(null);
+    const dragRegionSize = useSize(dragRegionRef);
+    const windowSize = useSize(windowWrapperRef);
 
     return (
-        <SemiConfigProvider locale={en_GB}>
-            <Layout hasSider className={'tw-h-screen'}>
-                <Sider className={'tw-min-[300px] tw-p-[15px]'}>
-                    <DragRegionOffset />
-                    <TodoApp />
-                </Sider>
-                <Resizer /> {/* todo: resize <Sider /> */}
-                <Layout>
-                    <Header className={'tw-font-bold tw-text-center'}>
-                        <DragRegionOffset />
-                        Header
-                        <EventController />
-                    </Header>
-                    <Content>
-                        {/* fixme: router outlet here which is not clear, too far separate with the React Router itself*/}
-                        <Outlet />
-                    </Content>
+        <div className={'tw-select-none tw-h-screen'} ref={windowWrapperRef}>
+            <DragRegionOffsetWrapper ref={dragRegionRef} />
+            <SemiConfigProvider locale={en_GB}>
+                <Layout
+                    hasSider
+                    style={{ height: (windowSize?.height || 1) - (dragRegionSize?.height || 0) }}
+                >
+                    <Sider
+                        className={'tw-min-[300px] tw-max-[600px] tw-p-[15px]'}
+                        style={{ width: siderWidth }}
+                        ref={siderRef}
+                    >
+                        <TodoApp />
+                    </Sider>
+                    <Resizer
+                        onMouseDown={() => setIsResizing(true)}
+                        onMouseUp={() => setIsResizing(false)}
+                    />
+                    <Layout>
+                        <Header className={'tw-font-bold tw-text-center'}>
+                            Header
+                            <EventController />
+                        </Header>
+                        <Content>
+                            {/* fixme: router outlet here which is not clear, too far separate with the React Router itself*/}
+                            <Outlet />
+                        </Content>
+                    </Layout>
                 </Layout>
-            </Layout>
-        </SemiConfigProvider>
+            </SemiConfigProvider>
+        </div>
     );
 }
