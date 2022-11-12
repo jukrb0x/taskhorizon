41d70d8c5a8921315c99ba43c4367b46039b4332
Jabriel <15688641+jukrb0x@users.noreply.github.com>
2022-11-13 07:57:17 +0800

    feat: drag and drop basic (not actually working)

--- src/components/Tasking/components/TodoList.tsx
+++ src/components/Tasking/components/TodoList.tsx
@@ -1,12 +1,16 @@
 import TodoItem from '@/components/Tasking/components/TodoItem';
 import { TodoStore } from '@/store/todo-store';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 
 export default function TodoList({ todos }: { todos: TodoStore[] }) {
     return (
         <>
-            {todos.map((todo, index) => {
-                return <TodoItem todo={todo} key={index} />;
-            })}
+            <DndProvider backend={HTML5Backend}>
+                {todos.map((todo, index) => {
+                    return <TodoItem todo={todo} key={index} />;
+                })}
+            </DndProvider>
         </>
     );
 }
