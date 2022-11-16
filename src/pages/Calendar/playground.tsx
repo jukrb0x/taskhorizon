052595d6e2bfa2d049471282c7d77ca1166a15e7
Jabriel <15688641+jukrb0x@users.noreply.github.com>
2022-11-17 07:13:47 +0800

    wip: event card

--- src/pages/Calendar/playground.tsx
+++ src/pages/Calendar/playground.tsx
@@ -1,13 +1,87 @@
 import { useState } from 'react';
-import { Checkbox } from '@mantine/core';
+import { Checkbox, Group, TextInput, Textarea, Select } from '@mantine/core';
 import { Button } from '@/components/Button';
+import { DatePicker, TimeInput } from '@mantine/dates';
+import { Input, TimePicker } from '@douyinfe/semi-ui';
+import * as dateFns from 'date-fns';
 
 const EventCard = () => {
+    const [title, setTitle] = useState<string>('');
+    const [startTime, setStartTime] = useState<Date | any>(new Date());
+    const [endTime, setEndTime] = useState<Date | any>(new Date());
     return (
         <>
-            <div className="tw-h-80 tw-w-96 tw-rounded-2xl tw-bg-white tw-drop-shadow-md">
-                <Checkbox />
-                <div className="tw-h-1/3 tw-w-full tw-rounded-t-2xl tw-bg-gray-100"></div>
+            <div className="tw-h-80 tw-w-96 tw-p-2 tw-rounded-2xl tw-bg-white tw-drop-shadow-lg">
+                <div className={'tw-flex tw-row-auto tw-items-center tw-space-x-1 tw-pb-1'}>
+                    <Checkbox size={'sm'} className={'tw-flex tw-justify-center'} />
+                    <TextInput
+                        variant={'unstyled'}
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                        placeholder={'New Event'}
+                        size={'xs'}
+                        styles={(theme) => ({
+                            root: {
+                                input: {
+                                    fontSize: theme.fontSizes.lg,
+                                    fontWeight: 700
+                                }
+                            }
+                        })}
+                    />
+                </div>
+                <div className={'tw-flex tw-row-auto tw-space-x-4'}>
+                    <div>
+                        <DatePicker
+                            placeholder={'start date'}
+                            inputFormat={'MMM DD YYYY'}
+                            clearable={false}
+                            variant={'filled'}
+                            size={'xs'}
+                        />
+                        <TimePicker
+                            style={{ display: 'block' }}
+                            showClear={false}
+                            minuteStep={30}
+                            value={startTime}
+                            onChange={(time) => setStartTime(time)}
+                            format={'HH:mm'}
+                            triggerRender={() => (
+                                <TimeInput
+                                    onChange={(time) => setStartTime(time)}
+                                    value={startTime}
+                                />
+                            )}
+                        />
+                    </div>
+                    <div>
+                        <DatePicker
+                            placeholder={'end date'}
+                            inputFormat={'MMM DD YYYY'}
+                            clearable={false}
+                            variant={'filled'}
+                            size={'xs'}
+                        />
+                        <TimePicker
+                            style={{ display: 'block' }}
+                            showClear={false}
+                            minuteStep={30}
+                            value={endTime}
+                            onChange={(time) => setEndTime(time)}
+                            format={'HH:mm'}
+                            triggerRender={() => (
+                                <TimeInput onChange={(time) => setEndTime(time)} value={endTime} />
+                            )}
+                        />
+                    </div>
+                </div>
+
+                <div>
+                    <Textarea variant={'filled'} placeholder={'Description'} radius={'sm'} />
+                </div>
+                {/*<div className=" tw-h-1/3 tw-w-full tw-rounded-t-2xl tw-bg-gray-100">
+    </div>
+    */}
             </div>
         </>
     );
