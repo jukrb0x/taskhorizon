5f6fef26a6b6912664b85525ae2751c908dfdaa3
Jabriel <15688641+jukrb0x@users.noreply.github.com>
2022-11-17 05:38:14 +0800

    fix: use controlled event instead of ref

--- src/components/Tasking/components/TodoInput.tsx
+++ src/components/Tasking/components/TodoInput.tsx
@@ -1,8 +1,7 @@
 import { TodoIdGenerator, Todo } from '@/store/todo-store';
 import { useTodoStore } from '@/store';
 import { useState } from 'react';
-import { TextInput, Button as MB } from '@mantine/core';
-import { useEventListener } from '@mantine/hooks';
+import { TextInput } from '@mantine/core';
 import { Button } from '@/components/Button';
 
 export default function TodoInput() {
@@ -19,52 +18,36 @@ export default function TodoInput() {
         addTodo && addTodo(todo);
         setTitle('');
     };
-    const inputRef = useEventListener('keydown', (event) => {
-        if (event.key === 'Enter') {
-            handleAdd();
-        }
-    });
 
     return (
         <>
             <div className={'tw-flex flex-row tw-gap-1.5 tw-mb-5'}>
                 <TextInput
                     className={'tw-flex-auto'}
-                    ref={inputRef}
+                    // ref={inputRef}
                     placeholder={'something to do...'}
                     onChange={(e) => setTitle(e.target.value)}
+                    onKeyDown={(e) => {
+                        if (e.key === 'Enter') {
+                            handleAdd();
+                        }
+                    }}
                     value={title}
                     styles={(theme) => ({
                         root: {
                             input: {
                                 '&:focus': {
-                                    borderColor: theme.colors.gray[5]
+                                    // borderColor: theme.colors.gray[5]
                                 }
                             }
                         }
                     })}
                 />
 
-                {/*
-                
-                <Input
-                    showClear
-                    placeholder={'something to do...'}
-                    onChange={(val) => setTitle(val)}
-                    value={title}
-                    onEnterPress={() => handleAdd()}
-                />
-                
-*/}
-                {/*
-
-                <Button theme={'solid'} onClick={() => handleAdd()}>
-                    Add
-                </Button>
-*/}
                 <Button
                     color={'blue'}
-                    variant={'outline'}
+                    variant={'filled'}
+                    disabled={title.trim() == ''}
                     className={'hover:tw-drop-shadow-sm'}
                     onClick={() => handleAdd()}
                 >
