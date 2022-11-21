f8a2af04d289401a1177ff3842494a162640e9b5
Jabriel <15688641+jukrb0x@users.noreply.github.com>
2022-11-22 07:18:10 +0800

    wip: auto adapted height `textarea`

--- src/components/Todo/components/Item.tsx
+++ src/components/Todo/components/Item.tsx
@@ -1,9 +1,10 @@
-import { Checkbox } from '@mantine/core';
+import { Text, Checkbox, Textarea } from '@mantine/core';
 
 import { Todo } from '@/store/todo-store';
 import { useTodoStore } from '@/store';
 
 import { useDrag } from 'react-dnd';
+import theme from 'tailwindcss/defaultTheme';
 
 export default function Item({ todo }: { todo: Todo }) {
     const { toggleTodo } = useTodoStore();
@@ -27,29 +28,55 @@ export default function Item({ todo }: { todo: Todo }) {
     return (
         <div
             ref={dragRef}
-            className={'tw-flex tw-items-center hover:tw-bg-gray-200 tw-mb-1 tw-p-1 tw-rounded-md'}
+            className={'tw-flex tw-items-start hover:tw-bg-gray-200 tw-mb-1 tw-p-1 tw-rounded-md'}
         >
             <Checkbox
+                className={'tw-mr-1.5'}
                 checked={todo.completed}
                 onChange={() => {
                     handleToggle();
                 }}
                 size={'xs'}
-                label={todo.title}
+                // label={todo.title}
                 styles={(theme) => ({
                     root: {
-                        display: 'flex',
                         input: {
                             borderRadius: theme.radius.sm
                         }
                     },
                     label: {
-                        fontSize: theme.fontSizes.sm,
-                        color: theme.colors.gray[7]
+                        fontSize: theme.fontSizes.sm
+                        // color: theme.colors.gray[7]
                     }
                 })}
             />
-            <div>{/*ac*/}</div>
+            <Text
+                component={'label'}
+                sx={(theme) => ({ fontSize: theme.fontSizes.sm, color: theme.colors.gray[7] })}
+            >
+                {todo.title}
+            </Text>
+
+            <Textarea
+                autosize
+                sx={(theme) => ({ fontSize: theme.fontSizes.sm, color: theme.colors.gray[7] })}
+                styles={(theme) => ({
+                    input: {
+                        padding: '0!important',
+                        fontSize: theme.fontSizes.sm,
+                        // color: theme.colors.gray[7],
+                        backgroundColor: 'transparent',
+                        // border: 'none',
+                        '&:focus': {
+                            outline: 'none'
+                        }
+                    }
+                })}
+                variant={'unstyled'}
+                // fs={'xs'}
+            >
+                {todo.title}
+            </Textarea>
         </div>
     );
 }
