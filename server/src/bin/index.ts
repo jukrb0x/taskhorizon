0a1878c389a3a50bf5bf28fedd83291c6a883e57
Jabriel <15688641+jukrb0x@users.noreply.github.com>
2022-12-08 00:17:36 +0800

    fix: update dockerfile

--- server/src/bin/index.ts
+++ server/src/bin/index.ts
@@ -1,11 +1,9 @@
 #!/usr/bin/env node
-import {CliCore} from "@tsed/cli-core";
-import {config} from "../config";
-import {HelloCommand} from "./HelloCommand";
+import { CliCore } from '@tsed/cli-core';
+import { config } from '../config';
+import { HelloCommand } from './HelloCommand';
 
 CliCore.bootstrap({
-  ...config,
-  commands: [
-    HelloCommand
-  ]
+    ...config,
+    commands: [HelloCommand]
 }).catch(console.error);
