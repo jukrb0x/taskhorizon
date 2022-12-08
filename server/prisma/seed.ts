de52c0d2eb0747db672d84d44bff732ae3acf671
Jabriel <15688641+jukrb0x@users.noreply.github.com>
2022-12-08 22:27:11 +0800

    feat: db prototyping

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -14,12 +14,71 @@ const users: Prisma.UserCreateInput[] = [
     }
 ];
 
+const todos: Prisma.TodoCreateInput[] = [
+    {
+        uuid: 'seeding-todo-1',
+        title: 'Prisma makes databases easy',
+        completed: false,
+        User: {
+            connect: {
+                email: 'alice@a.com'
+            }
+        },
+    },
+    {
+        uuid: 'seeding-todo-2',
+        title: 'Jabriel is a great wizard',
+        completed: false,
+        User: {
+            connect: {
+                email: 'bob@b.com'
+            }
+        }
+    }
+];
+
+const events: Prisma.EventCreateInput[] = [
+    {
+        uuid: 'seeding-event-1',
+        title: 'Jabriel is a great wizard',
+        start: new Date(),
+        end: new Date(),
+        description: 'haha',
+        allDay: false,
+        completed: false,
+        linkedTodos: {
+            connect: {
+                uuid: 'seeding-todo-2'
+            }
+        },
+        User: {
+            connect: {
+                email: 'bob@b.com'
+            }
+        }
+    }
+  ]
+
 async function main() {
     for (const user of users) {
         const newUser = await prisma.user.create({
             data: user
         });
         console.log(`Created new user: ${newUser.name} (ID: ${newUser.id})`);
+    }
+
+    for (const todo of todos) {
+        const newTodo = await prisma.todo.create({
+            data: todo
+        });
+        console.log(`Created new todo: ${newTodo.title} (ID: ${newTodo.id})`);
+    }
+
+    for (const event of events) {
+        const newEvent = await prisma.event.create({
+            data: event
+        });
+        console.log(`Created new event: ${newEvent.title} (ID: ${newEvent.id})`);
     }
     console.log('Seeding is Done.');
 }
