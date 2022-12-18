664de5f5c97574ac4070e2c91a6cc08fd6750981
Jabriel <15688641+jukrb0x@users.noreply.github.com>
2022-12-19 04:39:05 +0800

    wip: updates

--- client/src/components/AppShell/AppSider.tsx
+++ client/src/components/AppShell/AppSider.tsx
@@ -1,42 +1,61 @@
-import { ActionIcon, Divider, Menu } from '@mantine/core';
+import { ActionIcon, Divider, Group, Menu, Avatar, Text } from '@mantine/core';
 import {
     IconCalendar,
     IconCheckbox,
-    IconHome,
-    IconMessageCircle,
-    IconPackage,
-    IconPeace,
-    IconPhoto,
+    IconChevronRight,
     IconSettings,
     IconUserCircle
 } from '@tabler/icons';
 import clsx from 'clsx';
 import { useTauriExtension } from '@/hooks';
 import useAppConfigStore from '@/store/config-store';
-import { Button, Settings } from '@/components';
-import { MouseEventHandler, useState } from 'react';
+import { Settings } from '@/components';
+import { useState } from 'react';
+import exp from 'constants';
 
 const UserActionIcon = ({ onClick }: { onClick: () => void }) => {
     return (
         <>
             <Menu trigger={'hover'} openDelay={200} position={'right-end'}>
                 <Menu.Target>
-                    <ActionIcon size={'xl'} onClick={() => onClick()}>
+                    <ActionIcon size={'xl'} onClick={onClick}>
                         <IconUserCircle />
                     </ActionIcon>
                 </Menu.Target>
                 <Menu.Dropdown>
-                    <Menu.Label>Application</Menu.Label>
-                    <Menu.Item icon={<IconSettings size={14} />}>Settings</Menu.Item>
-                    <Menu.Item icon={<IconMessageCircle size={14} />}>Messages</Menu.Item>
-                    <Menu.Item icon={<IconPhoto size={14} />}>Gallery</Menu.Item>
+                    <Menu.Item onClick={onClick}>
+                        <Group>
+                            <Avatar
+                                src={
+                                    'https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=255&q=80'
+                                }
+                                radius="xl"
+                            />
+
+                            <div style={{ flex: 1 }}>
+                                <Text size="sm" weight={500}>
+                                    Harriette Spoonlicker
+                                </Text>
+
+                                <Text color="dimmed" size="xs">
+                                    asdad@sd.com
+                                </Text>
+                            </div>
+
+                            {<IconChevronRight size={16} />}
+                        </Group>
+                    </Menu.Item>
+                    <Menu.Divider />
+                    <Menu.Item icon={<IconSettings size={14} />} onClick={onClick}>
+                        Settings
+                    </Menu.Item>
                 </Menu.Dropdown>
             </Menu>
         </>
     );
 };
 
-const AppSider = () => {
+export const AppSider = () => {
     const isTauri = useTauriExtension();
     const { showSidebar, toggleSidebar } = useAppConfigStore();
     const [settingsOpen, setSettingsOpen] = useState(false);
@@ -72,4 +91,3 @@ const AppSider = () => {
         </>
     );
 };
-export { AppSider };
